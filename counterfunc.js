bb96a99510ed60e8250ae92ae7dcacbc9ae34d72
Ikram001 <ikramnixamani@gmail.com>
2025-01-26 01:31:51 +0500

    closure counter function

---
+++ counterfunc.js
@@ -0,0 +1,10 @@
+var createCounter = function(n) {
+    let current = n
+     return function() {
+         return current++
+     };
+ };
+ const counter = createCounter(10)
+  console.log(counter());
+  console.log(counter());
+  console.log(counter());
