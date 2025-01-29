2f4a5d57eb0221fe2304f2164258eddebc13a135
Ikram001 <ikramnixamani@gmail.com>
2025-01-30 00:58:35 +0500

    async await

--- syncvsasync.js
+++ syncvsasync.js
@@ -17,8 +17,33 @@ myPromise.then((message) => {
     console.log("This is in the catch " + message)
 })
 
-// function makeRequest(location){
-//     return new Promise((resolve, reject) => {
-//         console.log('making request to ${location}')
-//     })
-// }
+function makeRequest(location){
+    return new Promise((resolve, reject) => {
+        console.log(`making request to ${location}`)
+        if(location === "Google"){
+            resolve("Google says Hi")
+        }else{
+            reject("We can only talk to google")
+        }
+    })
+}
+
+function processRequest(response){
+    return new Promise ((resolve, reject) => {
+        console.log("Processing responese")
+        resolve(`Extra information ${response}`)
+    })
+}
+
+async function dowork() {
+    try{
+        const response = await makeRequest("aaa")
+        console.log("Response Received")
+        const processResponse = await processRequest(response)
+        console.log(processResponse)
+    }catch(error){
+        console.log(error)
+    }
+}
+
+dowork()
