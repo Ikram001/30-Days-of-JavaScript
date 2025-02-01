4e614a99e51023a7537d53c66ee31cd9c260c226
Ikram001 <ikramnixamani@gmail.com>
2025-02-01 15:34:10 +0500

    problem

--- 2625-intervals.js
+++ 2625-intervals.js
@@ -1,10 +1,21 @@
+// function name(){
+//     console.log("ikram")
+// }
+// let oneSecond = setInterval(name,1000)
 
-function name(){
-    console.log("ikram")
-}
-let oneSecond = setInterval(name,1000)
+
+// setTimeout(() => {
+//     (clearInterval(oneSecond,1000))
+// }, 5000);
 
 
-setTimeout(() => {
-    (clearInterval(oneSecond,1000))
-}, 5000);
+var cancellable = function(fn, args, t) {
+    fn(...args)
+    let delayTimer = setInterval(() => {
+        fn(...args)
+    },t)
+    let cancelFn = () => {
+        clearInterval(delayTimer)
+    }
+    return cancelFn
+};
